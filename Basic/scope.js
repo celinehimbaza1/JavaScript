3752b1bc62e5be10feaea8115f1448d29218059a
Celine Himbaza <celine.himbaza@gmail.com>
2024-11-11 17:30:06 +0200

    variable-scope

---
+++ Basic/scope.js
@@ -0,0 +1,12 @@
+// loxical scope (static scope)
+// Global variabe is defined outside of all code blocks
+// Local variable defined inside a code block
+
+let varone= 'varOn' // Global variable
+ 
+if (true){
+    console.log(varon)
+    let vartwo='vartwo' // Local variable
+   
+}
+console.log(vartwo) //  vartwo is not defined
