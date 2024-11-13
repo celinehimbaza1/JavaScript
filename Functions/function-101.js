6c32854958fe1e5dd6a28f7d1eedd92a2e7e460b
Celine Himbaza <celine.himbaza@gmail.com>
2024-11-13 05:38:19 +0200

    jsFunction

---
+++ Functions/function-101.js
@@ -0,0 +1,22 @@
+// function= input(argument),code,output(return value)
+
+
+// function definition
+let greetUser= function(){
+    console.log('Welcome user')
+
+}
+
+// function call
+greetUser() 
+greetUser() 
+
+let square = function (num){
+    let result= num * num
+    return result
+}
+ let value1 = square(5)
+ let value2 = square(4)
+
+ console.log(value1)
+ console.log(value2)
