3a6161d0e79045b98afbfbbe1ca2361c03e73b16
Celine Himbaza <celine.himbaza@gmail.com>
2024-11-14 19:46:00 +0200

    VariableScope

---
+++ Basic/scope-2.js
@@ -0,0 +1,12 @@
+let name='Celine'
+
+if(true){
+    if(true){
+        let name ='Jane' 
+        if(true){
+             name ='John'
+        }
+        console.log(name)
+    }
+}
+console.log(name)
---
+++ Object/objDeclaration.js
@@ -0,0 +1,17 @@
+//Normal declaration
+  // let name= 'Celine'
+  // let age= 27
+
+//Object declaration
+
+let person = {
+    name:'Celine',
+    age: 27
+};
+
+//Changing name we use  Dot notation
+person.name='Marry'
+
+// Bracket notation
+person['name']='Mary'
+console.log(person.name)
