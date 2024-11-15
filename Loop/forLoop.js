9435315de77957727db071b239903b9fd6d136a7
Celine Himbaza <celine.himbaza@gmail.com>
2024-11-15 13:55:47 +0200

    forLoop

---
+++ Loop/forLoop.js
@@ -0,0 +1,18 @@
+// Loop is used to repeat action several time
+ // For loop
+  
+
+ // This will display Hello world 5 times
+//  for (let i=0; i<5; i++){
+//     console.log('Hello world!')
+//  }
+
+
+// Display odd number
+ for (let i=0; i<5; i++){
+    if (i % 2 ===1){
+        console.log(i)
+    }
+ }
+
+ 
