5eb0648246f252e94ab3c4722539f7594c03bd60
Celine Himbaza <celine.himbaza@gmail.com>
2024-11-15 13:15:08 +0200

    arrayIntro

---
+++ Array/array101.js
@@ -0,0 +1,8 @@
+let selectedColor = ['red','white','green']
+selectedColor[3]= 6
+
+console.log(selectedColor[1])
+
+// checking element in array we use lenght property
+
+console.log(selectedColor.length)
