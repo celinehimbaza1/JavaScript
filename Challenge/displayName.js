e83c21d4887498caa0fd5098ae0bee9023e9ca01
Celine Himbaza <celine.himbaza@gmail.com>
2024-11-22 08:30:38 -0800

    displayName

---
+++ Challenge/displayName.js
@@ -0,0 +1,9 @@
+function displayNames(firstName,lastName,age){
+
+    let fullname= firstName + ' ' + lastName + ' ' + 'with age' + ' '+ age
+    return fullname
+}
+
+ let myInfo= displayNames('Celine','Himbaza',27)
+ 
+console.log('My name is '+ myInfo)
