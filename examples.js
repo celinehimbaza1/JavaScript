06f38e8864513d58ef28fc0e61bcddb57049f981
Celine Himbaza <celine.himbaza@gmail.com>
2024-12-29 11:39:09 +0200

    Practice

--- examples.js
+++ examples.js
@@ -8,3 +8,11 @@ let mul= mult(3,2)
 let result = add(3,4)
 console.log('the sum is:' +result)
 console.log('the mult is:' +mul)
+
+const person={
+    name:'celine',
+    address:'kanombe',
+    weight:59,
+    age:10
+}
+console.log('my name is ' + person.name + ' ' + 'i have' + ' ' +person.age + ' ' + 'years old')
