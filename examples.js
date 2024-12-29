17a0e2361a9a29f5f567275014b6df565e737768
Celine Himbaza <celine.himbaza@gmail.com>
2024-12-29 11:03:07 +0200

    Practice

---
+++ examples.js
@@ -0,0 +1,10 @@
+function add(a,b){
+    return a+b;
+}
+function mult(a,b){
+    return a*b
+}
+let mul= mult(3,2)
+let result = add(3,4)
+console.log('the sum is:' +result)
+console.log('the mult is:' +mul)
