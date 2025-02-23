65f1bd26e504d6665f8cd63609a02e80203ebe3b
Celine Himbaza <celine.himbaza@gmail.com>
2025-02-23 12:06:59 +0200

    assessment

---
+++ assessment.js
@@ -0,0 +1,34 @@
+// for (let i=1;i<=100;i++){
+//     if(i % 7===0 || (i.toString().includes('7'))){
+//         console.log("Lucky!")
+//     }
+//     else{
+//         console.log(i)
+//     }
+ 
+// }
+
+
+// function solution(n) {
+//     return Math.floor(n / 10) + (n % 10)
+
+// }
+
+// const result = solution(12)
+
+// console.log(result)
+
+
+
+<div class="container">
+    <button type="button">
+        Click Me!
+    </button>
+    <div class="counter">
+        <span class="counter-label">Times Clicked:</span>
+        <span class="counter-value">0</span>
+    </div>
+</div>
+
+
+
